bb8ec883653739365386be20847af73b87fc5fe7
MOVZX <movzx@yahoo.com>
2022-10-29 13:24:57 +0700

    Various Changes
    - Bump version number
    - Add Ultra Quality mode
    - Sync Auto Exposure
    
    Signed-off-by: MOVZX <movzx@yahoo.com>

--- CyberFSR/NvParameter.cpp
+++ CyberFSR/NvParameter.cpp
@@ -164,6 +164,7 @@ void NvParameter::Set_Internal(const char* InName, unsigned long long InValue, N
 		DepthInverted = *inValueInt & NVSDK_NGX_DLSS_Feature_Flags_DepthInverted;
 		JitterMotion = *inValueInt & NVSDK_NGX_DLSS_Feature_Flags_MVJittered;
 		LowRes = *inValueInt & NVSDK_NGX_DLSS_Feature_Flags_MVLowRes;
+		AutoExposure = *inValueInt & NVSDK_NGX_DLSS_Feature_Flags_AutoExposure;
 		break;
 	case Util::NvParameter::DLSS_Input_Bias_Current_Color_Mask:
 		InputBiasCurrentColorMask = inValuePtr;
@@ -310,7 +311,9 @@ void NvParameter::EvaluateRenderScale()
 		fsrQualityMode = FFX_FSR2_QUALITY_MODE_ULTRA_PERFORMANCE;
 		break;
 	case NVSDK_NGX_PerfQuality_Value_UltraQuality:
-		//Not defined by AMD
+		fsrQualityMode = FFX_FSR2_QUALITY_MODE_ULTRA_QUALITY;
+		break;
+	default:
 		OutHeight = Height;
 		OutWidth = Width;
 		return;
